d1e10922df322562b8c99c1c31bc58e2da712ff2
Jason <jasonnchenn13@gmail.com>
2020-04-30 04:20:02 -0400

    Add Label

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jason Chen on 4/30/20.
+//  Copyright © 2020 Jason Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
